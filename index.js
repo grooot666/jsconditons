f6c94d957ca6951724aef5d30bdd19c97c1477a4
24LP4133 24LP4133 <24LP4133@G304-CC0218.local>
2024-12-27 20:21:12 +0800

    exercise

--- index.js
+++ index.js
@@ -5,12 +5,12 @@
 // Sample Input: let number = -5;
 // Sample Output: "-5 is negative"
 
-let n = 5
-if(n > 0 ){
-    console.log(n + "positive");
+// let n = 5
+// if(n > 0 ){
+//     console.log(n + "positive");
     
-}else {console.log(n + "negative");
-}
+// }else {console.log(n + "negative");
+// }
  
 // 2
  
@@ -19,17 +19,17 @@ if(n > 0 ){
 // Sample Input: let temperature = 15;
 // Sample Output: "15°C is normal"
 
-let Celsius = -3
-if( Celsius < 0){
-    console.log("freezing");
+// let Celsius = -3
+// if( Celsius < 0){
+//     console.log("freezing");
     
-}else if ( Celsius >= 0 && Celsius <= 30 ){
-    console.log("normal");
+// }else if ( Celsius >= 0 && Celsius <= 30 ){
+//     console.log("normal");
     
-}else {
-    console.log("hot");
+// }else {
+//     console.log("hot");
     
-}
+// }
  
 // 3
  
@@ -38,32 +38,24 @@ if( Celsius < 0){
 // Sample Output: "a is a vowel"
 
 
-let char = 'e'
-// char = char.toLowerCase
-// let vowel = ['a', 'e', 'i', 'o', 'u'];
-// if(vowel.includes(char)){
-//     console.log(char + "is vowel");
+// let char = 'e'
+
+
+// if(char ==='a'){
+//     console.log(char + " is vowel");
     
+// }else if(char ==='e'){
+//     console.log(char + " is vowel");
+// }else if(char ==='i'){
+//     console.log(char + " is vowel");
+// }else if(char ==='o'){
+//     console.log(char + " is vowel");
+// }else if(char ==='u'){
+//     console.log(char + " is vowel");
 // }else{
 //     console.log("is consonant");
     
 // }
-
-if(char ==='a'){
-    console.log(char + " is vowel");
-    
-}else if(char ==='e'){
-    console.log(char + " is vowel");
-}else if(char ==='i'){
-    console.log(char + " is vowel");
-}else if(char ==='o'){
-    console.log(char + " is vowel");
-}else if(char ==='u'){
-    console.log(char + " is vowel");
-}else{
-    console.log("is consonant");
-    
-}
     
 
 
@@ -112,47 +104,50 @@ if(char ==='a'){
  
 // Sample Input: let age = 16;
 // Sample Output: "You are a teenager"
-let age = 18
-if( age < 13){
-    console.log("child");
+
+
+// let age = 18
+// if( age < 13){
+//     console.log("child");
     
-}else if(age >=13 && age <=19){
-    console.log("teenager");
+// }else if(age >=13 && age <=19){
+//     console.log("teenager");
     
-}else if(age >= 20){
-    console.log("adult");
+// }else if(age >= 20){
+//     console.log("adult");
     
-}   
+// }   
  
 // 6
  
 // Write a JavaScript program to check if a given number is divisible by 5 or 7.
 // Sample Input: let number = 35;
 // Sample Output: "35 is divisible by both 5 and 7"
-let e = 30
-if(e % 5 === 0 && e % 7 === 0){
-    console.log("mon");
+
+// let e = 30
+// if(e % 5 === 0 && e % 7 === 0){
+//     console.log("mon");
 
     
-}else{
-    console.log("bish");
+// }else{
+//     console.log("bish");
     
-}
+// }
  
 // 7
  
 
-let time = 19
-if(time >= 0 && time <= 11){
-    console.log("morning");
+// let time = 19
+// if(time >= 0 && time <= 11){
+//     console.log("morning");
     
-}else if(time >= 12 && time <= 17){
-    console.log("afternoun");
+// }else if(time >= 12 && time <= 17){
+//     console.log("afternoun");
     
-}else if(time >= 18 && time <= 23){
-    console.log("evening");
+// }else if(time >= 18 && time <= 23){
+//     console.log("evening");
     
-}
+// }
  
 // 8
  
@@ -160,34 +155,87 @@ if(time >= 0 && time <= 11){
 // Sample Input: let number = 25;
 // Sample Output: "25 is a double-digit number"
  
+
+//  let p = prompt()
+//  if (p >= 100 && p < 1000 ){
+//     console.log("gurwan orontoi too");
+    
+//  }else if(p >= 10 && p < 100){
+//     console.log("hoyor orontoi");
+    
+//  }else if( p >= 10 && p < 10){
+//     console.log( "neg orontoi");
+    
+//  }else{
+//     console.log(null);
+    
+//  }
 // 9
  
 // Write a JavaScript program to determine whether a number is a multiple of 4.
 // Sample Input: let number = 16;
 // Sample Output: "16 is a multiple of 4"
  
+// let num = prompt()
+// if(num % 4 === 0){
+//     console.log("mon");
+    
+// }else{
+//     console.log("bish");
+    
+// }
+
 // 10
  
 // Write a JavaScript program to check if a given character is uppercase or lowercase.
 // Sample Input: let char = 'G';
 // Sample Output: "G is uppercase"
- 
+
+// let char = prompt()
+// if(char === char.toUpperCase()){
+//     console.log("Tom");
+
+    
+// }else if ( char === char.toLowerCase()){
+//     console.log("jijig");
+    
+// }
 // 11
  
 // Write a JavaScript program to determine if a given day number corresponds to a weekday or a weekend (1 = Monday, ..., 7 = Sunday).
 // Sample Input: let day = 6;
 // Sample Output: "It's a weekend"
+// let num = prompt()
+// if(num >= 1 && num <=5) {
+//     console.log("ene bol ajliiin odr");
+    
+// }else if(num >= 6 && num <= 7 ){
+//     console.log("ene bol amraltiin odr");
+    
+// }
  
 // 12
  
 // You’re designing a coffee shop menu. Write a program to determine the price of a coffee based on size:
  
-// Small: $2
+// Small: $2 
 // Medium: $3
 // Large: $4
  
 // Sample Input: let size = 'Medium';
 // Sample Output: "A Medium coffee costs $3"
+
+// let price = prompt()
+// if(price == 3){
+//  console.log(" a small coffe 3$");
+ 
+// }else if(price == 4 ){
+//     console.log(" a medium  coffe 4$");
+    
+// }else if( price == 5){
+//     console.log("a large coffe 5$");
+    
+// }
  
 // 13
  
@@ -196,6 +244,19 @@ if(time >= 0 && time <= 11){
 // Sample Input: let weather = 'Rainy';
 // Sample Output: "Don't forget your umbrella!"
  
+
+// let weather = prompt()
+// if(weather == "rainy"){
+//     console.log("don't forget your umbrella");
+    
+// }else if( weather == "sunny"){
+//     console.log("don't forget your sunglasses");
+    
+// }else if(weather == "cold"){
+//     console.log("don't forget your warm clothes");
+    
+// }else{console.log(null);
+// }
 // 14
  
 // You are creating a program for a theater. Based on a person's age, determine their ticket price:
@@ -205,6 +266,18 @@ if(time >= 0 && time <= 11){
 // 19 to 60: $15
 // Above 60: $12
  
+// let age = prompt()
+// if(age < 5){
+//     console.log("free");
+    
+// }else if( age >= 5 && age <= 18){
+//     console.log("10$");
+    
+// }else if( age >= 19 && age <= 60){
+//     console.log("15$");
+    
+// }else{console.log("12$");
+// }
 // Sample Input: let age = 70;
 // Sample Output: "Your ticket price is $12"
  
@@ -215,6 +288,16 @@ if(time >= 0 && time <= 11){
 // Energy > 70: Happy
 // Energy between 30 and 70: Hungry
 // Energy < 30: Sleepy
+
+// let energy = prompt()
+// if(energy > 70){
+//     console.log("happy");
+    
+// }else if( energy >= 30 && energy <= 70){
+//     console.log("hungry");
+    
+// }else{console.log("sleepy");
+// }
  
 // Sample Input: let energy = 25;
 // Sample Output: "Your pet is Sleepy"
@@ -229,6 +312,18 @@ if(time >= 0 && time <= 11){
  
 // Sample Input: let weight = 75;
 // Sample Output: "You are lifting at an Intermediate level"
+
+// let weight = prompt()
+// if(weight < 50 ){
+//     console.log(" you are lifting at an begginer level");
+    
+// }else if(weight >= 50 && weight <= 100){
+//     console.log( "You are lifting at an Intermediate level");
+    
+// }else{
+//     console.log( "You are lifting at an advanced level");
+    
+// }
  
 // 17
  
@@ -243,7 +338,20 @@ if(time >= 0 && time <= 11){
 // Sample Output: "You get a 20% discount"
  
 // 18
- 
+// let amount = prompt()
+// if( amount < 50){
+//     console.log("No discount");
+    
+// }else if( amount >= 50 && amount <= 100){
+//     console.log(" 10% discount");
+    
+// }else if ( amount >= 101 && amount <= 200){
+//     console.log("20% discount");
+    
+// }else {
+//     console.log("30% discount");
+    
+// }
 // Write a program to determine if a password is strong, medium, or weak.
  
 // Strong: At least 8 characters, includes numbers and special characters
@@ -253,6 +361,18 @@ if(time >= 0 && time <= 11){
 // Sample Input: let password = 'Pass123!';
 // Sample Output: "Your password is strong"
  
+
+// let password = prompt()
+// if(password.length > [8]){
+//     console.log("Your password is strong");
+    
+// }else if(password.length == [6]){
+//     console.log("Your password is medium");
+    
+// }else if(password.length < [6]){
+//     console.log("Your password is weak");
+    
+// }
 // 19
  
 // You’re coding a program for an amusement park. Ticket prices vary by age and height:
@@ -263,7 +383,20 @@ if(time >= 0 && time <= 11){
  
 // Sample Input: let age = 65, height = 130;
 // Sample Output: "Your ticket price is $7"
- 
+
+// let height = prompt('input height')
+// let age = prompt('input age')
+// let ticketPrice;
+// if(height < 120){
+//     console.log(ticketPrice = "5$");
+    
+// }else if(height >= 120 || age > 60){
+//     console.log(ticketPrice ="10$");
+    
+// }else{
+//     console.log(ticketPrice = "7$");
+    
+// }
 // 20
  
 // Write a program for a mobile data plan. Determine the cost based on data usage:
@@ -276,6 +409,18 @@ if(time >= 0 && time <= 11){
 // Sample Input: let dataUsage = 25;
 // Sample Output: "Your data cost is $30"
  
+// let data = prompt("Input Data")
+// if (data <= 5){
+//     console.log("$10");
+    
+// }else if( data > 5 && data <= 20){
+//     console.log("$20");
+    
+// }else if( data > 20 && data <= 50){
+//     console.log("$30");
+    
+// }else{console.log("$50");
+// }
 // 21
  
 // Write a program for a bank loan eligibility check. A user qualifies if:
@@ -286,7 +431,15 @@ if(time >= 0 && time <= 11){
  
 // Sample Input: let age = 30, income = 30000, creditScore = 650;
 // Sample Output: "You are eligible for the loan"
- 
+let age = 18
+let annualIncome = 50000
+let CreditScore = 800
+
+if( age >= 21 && annualIncome >= 25000 && CreditScore > 600){
+    console.log("You are eligible for the loan");
+    
+}else console.log("no");
+
 // 22
  
 // Write a program for an online grading system where the final grade depends on a combination of attendance and exam scores:
@@ -298,7 +451,24 @@ if(time >= 0 && time <= 11){
 // Average >= 70: C
 // Average >= 60: D
 // Below 60: F
- 
+
+let average = 84
+if( average >= 90){
+    console.log("A");
+    
+} else if( average >= 80){
+    console.log("B");
+    
+}else if( average >= 70){
+    console.log("c");
+    
+}else if( average >= 60){
+    console.log("d");
+    
+}else if( average < 60 ){
+    console.log(" F");
+    
+}
 // Sample Input: let attendance = 80; let exam1 = 85
 // Sample Output: "Your final grade is B"
  
